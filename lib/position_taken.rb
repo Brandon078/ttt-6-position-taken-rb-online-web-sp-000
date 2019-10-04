96501db575aac9519a1ef3676b058a0cdfa828bd
brandon moro <brandon_078@hotmail.com>
2019-10-04 02:02:43 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
-def position_taken(board, position)
-  if (board[position] == " ")
+def position_taken(board, input)
+  if (board[input] == " ")
     puts "False Aka empty"
   end
 end
