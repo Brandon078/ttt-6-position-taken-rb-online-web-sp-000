e15dc07f0e89910d1b21a5ec45726c670560ed67
brandon moro <brandon_078@hotmail.com>
2019-10-04 02:09:04 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board, input)
   if (board[index] == " " || board[index] == "" || board[index] == nil)
-    puts "False Aka empty"
+    puts "False"
   else
     puts "True"
   end
